a523a69b0763f34a9b119fa5c71b1b43f2d77dbe
Manik Shah <marsleonz@gmail.com>
2024-07-19 20:44:00 +0545

    Integrate db with the frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
 import { Button } from "@/components/ui/button";
-
+import Image from "next/image";
+import PatientForm from "@/components/forms/PatientForm";
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-white text-3xl underline">Home</h1>
-      <Button>Click Me</Button>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />{" "}
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePluse
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
